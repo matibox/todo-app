c15e19c595caea6a318e9dddaeb19fdeed5bc871
Matibox <mateusz.hladky@gmail.com>
2022-06-22 15:04:31 +0200

    Add basic HTML structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+// Font awesome
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
 // Styles
 import './styles/main.scss';
 
 // Assets
 import assets from './assets';
 assets();
+
+// ==== Mode Switch ====
+// Preferred scheme
+if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+    document.body.classList.add('dark');
+}
