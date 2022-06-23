b3dfefeca375a736bc90548f7e0aa486874c02e7
Matibox <mateusz.hladky@gmail.com>
2022-06-23 11:51:47 +0200

    Style header and new todo

--- src/assets.js
+++ src/assets.js
@@ -1,3 +1,10 @@
+// Backgrounds
+import bgDD from './assets/bg-desktop-dark.jpg';
+import bgDL from './assets/bg-desktop-light.jpg';
+import bgMD from './assets/bg-mobile-dark.jpg';
+import bgML from './assets/bg-mobile-light.jpg';
+
+// Icons
 import iconMoon from './assets/icon-moon.svg';
 import iconSun from './assets/icon-sun.svg';
 import iconCross from './assets/icon-cross.svg';
@@ -8,6 +15,10 @@ export default function assets() {
         { assetName: iconMoon, element: 'moon' },
         { assetName: iconSun, element: 'sun' },
         { assetName: iconCross, element: 'remove' },
+        { assetName: bgDD, element: 'bg-d-d' },
+        { assetName: bgDL, element: 'bg-d-l' },
+        { assetName: bgMD, element: 'bg-m-d' },
+        { assetName: bgML, element: 'bg-m-l' },
     ];
 
     function setAssetsSource(assets) {
--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ import assets from './assets';
 assets();
 
 // ==== Mode Switch ====
+const header = document.querySelector('[data-header]');
+const newTodoContainer = document.querySelector('[data-new-todo-container]');
+const insertBtn = document.querySelector('[data-insert]');
+const newTodo = document.querySelector('[data-new-todo]');
+
 // Preferred scheme
 if (
     window.matchMedia &&
     window.matchMedia('(prefers-color-scheme: dark)').matches
 ) {
     document.body.classList.add('dark');
+    header.classList.add('app__header--dark');
+    newTodoContainer.classList.add('app__new-todo-container--dark');
+    insertBtn.classList.add('app__new-todo-insert--dark');
+    newTodo.classList.add('app__new-todo--dark');
 }
